fb534c8778fa69e59c71a5a7645917e57dde412d
Dominik <unbekannt112358132143@web.de>
2016-06-22 19:49:30 +0200

    Added GUI-template

--- src/tddt/TDDT.java
+++ src/tddt/TDDT.java
@@ -1,5 +1,27 @@
 package tddt;
 
-public class TDDT {
-	//TODO everything
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TDDT extends Application{
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/tddt/layout_v1.fxml"));
+
+			Scene scene = new Scene(root, 500, 500);
+
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
