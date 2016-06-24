2014e68e78b70a3232a56f612c57b414fd2cd935
Dominik <unbekannt112358132143@web.de>
2016-06-24 20:17:55 +0200

    Remove later. Testing testing with travis

--- src/test/Test_X.java
+++ src/test/Test_X.java
@@ -1,5 +1,12 @@
 package test;
 
-public class Test_X {
+import static org.junit.Assert.*;
 
+import org.junit.Test;
+
+public class Test_X {
+	@Test
+	public void AlwaysTrue() {
+		assertEquals(0, 0);
+	}
 }
