b0b6edbc8534aaa53bc347a65764074b55fddc54
Dominik <unbekannt112358132143@web.de>
2016-06-25 23:49:15 +0200

    created ControllerTest

---
+++ src/test/java/ControllerTest.java
@@ -0,0 +1,14 @@
+package test.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ControllerTest {
+//TODO all
+	@Test
+	public void test() {
+		assertEquals(1, 1);
+	}
+
+}
