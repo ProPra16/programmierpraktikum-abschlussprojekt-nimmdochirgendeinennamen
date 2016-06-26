16b8e913707ab6528ad595fe2a364329d151505f
jule34524 <jule_p_@live.de>
2016-06-26 17:28:05 +0200

    moved tracker to src

---
+++ src/tracker/TrackerInterface.java
@@ -0,0 +1,24 @@
+/*
+*Measures time spent in a certain phase. 
+*When the phase is ended, writes the time to a textfile.
+*/
+package tracker;
+
+public interface TrackerInterface {
+    
+    //call when changing to next phase
+    public void nextPhase();
+    
+    //call when changing BACK from GREEN to RED
+    public void greenBack();
+    
+    //following methods are only used by the implementing class and should not 
+    //be called outside of it
+    public void redToGreen();
+    
+    public void greenToRefactor();
+    
+    public void refactorToRed();
+    
+    public void writeToFile();
+}
