17ca15d33fdc34e48ae7a6855baf6bf25e741ed5
Kai <git@holzinger-it.de>
2016-06-26 15:35:15 +0200

    XMLLoader
    Standard XML file with correct catalog syntax and XML loader interface

---
+++ src/xmlLoader/LoaderInterface.java
@@ -0,0 +1,31 @@
+package xmlLoader;
+
+public interface LoaderInterface {
+
+    //load a catalog from an xml file.
+    void loadCatalog();
+
+    //@return ID of current node
+    int getID();
+
+    //@return Name of the node with passed ID
+    String getNameByID(int id);
+
+    //@return description of the node with passed ID
+    String getDescriptionByID(int id);
+
+    //@return all classes to be found for the node with passed ID
+    String[] getClassesByID(int id);
+
+    //@return all tests to be found for the node with passed ID
+    String[] getTestsByID(int id);
+
+    //@return true if babysteps is active for the node with passed ID
+    boolean isBabystepsActive(int id);
+
+    //@return true if timetracker is active for the node with passed ID
+    boolean isTimetrackerActive(int id);
+
+    //@return total amount of nodes which have been loaded from the catalog file.
+    int getNumberOfExercises();
+}
