5a4aaba376cfd697e3776e5c759ff3408dfbdcce
Kai <git@holzinger-it.de>
2016-06-27 12:47:43 +0200

    update
    method name adjustments, added some text to description in the xml to
    make it more clear

--- src/xmlLoader/LoaderInterface.java
+++ src/xmlLoader/LoaderInterface.java
@@ -5,23 +5,23 @@ public interface LoaderInterface {
     //load a catalog from an xml file.
     void loadCatalog();
 
-    //@return Name of the node with passed ID
-    String getNameByID(int id);
+    //@return Name of the node with passed IDX
+    String getName(int idx);
 
-    //@return description of the node with passed ID
-    String getDescriptionByID(int id);
+    //@return description of the node with passed IDX
+    String getDescription(int idx);
 
-    //@return all classes to be found for the node with passed ID
-    String[] getClassesByID(int id);
+    //@return all classes to be found for the node with passed IDX
+    String[] getClasses(int idx);
 
-    //@return all tests to be found for the node with passed ID
-    String[] getTestsByID(int id);
+    //@return all tests to be found for the node with passed IDX
+    String[] getTests(int idx);
 
-    //@return true if babysteps is active for the node with passed ID
-    boolean isBabystepsActive(int id);
+    //@return true if babysteps is active for the node with passed IDX
+    boolean isBabystepsActive(int idx);
 
-    //@return true if timetracker is active for the node with passed ID
-    boolean isTimetrackerActive(int id);
+    //@return true if timetracker is active for the node with passed IDX
+    boolean isTimetrackerActive(int idx);
 
     //@return total amount of nodes which have been loaded from the catalog file.
     int getNumberOfExercises();
