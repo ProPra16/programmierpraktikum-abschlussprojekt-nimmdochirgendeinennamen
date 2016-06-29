59c131c76a59241e6eb13af75294525430edd7f1
Dominik <unbekannt112358132143@web.de>
2016-06-29 17:55:40 +0200

    refactored and wrapped FileReader for TDDTTask and TDDTTracker

--- src/main/java/TDDTFileReader.java
+++ src/main/java/TDDTFileReader.java
@@ -2,11 +2,12 @@ package main.java;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TDDTFileReader {
 
-	public String read(File file) {
+	public String readAll(File file) {
 		StringBuilder content = new StringBuilder();
 		try {
 			List<String> lines = Files.readAllLines(file.toPath());
@@ -17,6 +18,19 @@ public class TDDTFileReader {
             e.printStackTrace();
 		}
 		return content.toString();
+	}
+
+	public String[] readLines(File file) {
+		ArrayList<String> list = new ArrayList<String>();
+		try {
+			List<String> lines = Files.readAllLines(file.toPath());
+			for (String line : lines)
+				list.add(line);
+
+		} catch (Exception e) {
+            e.printStackTrace();
+		}
+		return list.toArray(new String[list.size()]);
 	}
 
 }
