3bd740d7481bc9f182c58c9025e5716dcdc194ea
jule34524 <jule_p_@live.de>
2016-07-06 14:31:34 +0200

    License Header, improved code

--- src/tracker/ChartTracker.java
+++ src/tracker/ChartTracker.java
@@ -1,29 +1,43 @@
 /*
-*Measures time spent in a certain phase. 
-*When the phase is ended, writes the time (in seconds) to a textfile.
-*
-*Call constructor to start, nextPhase() to change to the next phase, 
-*greenBack() to go back from GREEN to RED.
-*Time will be measured and written automatically.
-*/
+ * Copyright (c) 2016. Caro Jachmann, Dominik Kuhnen, Jule Pohlmann, Kai Brandt, Kai Holzinger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
 package tracker;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
 public class ChartTracker implements ChartTrackerInterface {
+    /*
+     * Measures time spent in a certain phase.
+     * When the phase is ended, writes the time (in seconds) to a textfile.
+     *
+     * Call constructor to start, nextPhase() to change to the next phase,
+     * greenBack() to go back from GREEN to RED.
+     * Time will be measured and written automatically.
+     */
+
     //seconds spent in each phase so far
-    int greenSeconds;
-    int redSeconds; 
-    int refactorSeconds;
+    private int greenSeconds;
+    private int redSeconds;
+    private int refactorSeconds;
     
     //system time when phase is entered
-    long greenStartTime;
-    long redStartTime;
-    long refactorStartTime;
+    private long greenStartTime;
+    private long redStartTime;
+    private long refactorStartTime;
     
     //current phase (0 = RED, 1 = GREEN, 2 = REFACTOR)
-    int phase;
+    private int phase;
            
     public ChartTracker() {
         //initialise times to 0
@@ -34,7 +48,7 @@ public class ChartTracker implements ChartTrackerInterface {
         //start in RED phase
         this.phase = 0;
         
-        redStartTime = System.currentTimeMillis();
+        this.redStartTime = System.currentTimeMillis();
     }
     
     //change to next phase, automatically updates phase
@@ -61,29 +75,28 @@ public class ChartTracker implements ChartTrackerInterface {
         }
     }
 
-    //following methods are only used by this class and should not 
-    //be called outside of it
-     
-    public void redToGreen() {
+                                        /*INTERNAL METHODS*/
+
+    private void redToGreen() {
         long redEndTime = System.currentTimeMillis() - redStartTime;
         redSeconds += (int)redEndTime/1000;
         greenStartTime = System.currentTimeMillis();
     }
     
-    public void greenToRefactor() {
+    private void greenToRefactor() {
         long greenEndTime = System.currentTimeMillis() - greenStartTime;
         greenSeconds += (int)greenEndTime/1000;
         refactorStartTime = System.currentTimeMillis();
     }
     
-    public void refactorToRed() {
+    private void refactorToRed() {
         long refactorEndTime = System.currentTimeMillis() - refactorStartTime;
         refactorSeconds += (int)refactorEndTime/1000;
         redStartTime = System.currentTimeMillis();
     }
 
     //write to file:
-    public void writeToFile() {
+    private void writeToFile() {
         try (PrintWriter printFile = new PrintWriter("TrackingData.txt")) {
             printFile.println("red\n" + redSeconds);
 	    printFile.println("green\n" + greenSeconds);
