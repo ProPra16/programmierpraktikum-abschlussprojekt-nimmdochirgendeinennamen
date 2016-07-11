f6a0e6132b7fefd7981b7381295f5f69e74b3f31
KaiBrandt <kabra110@uni-duesseldorf.de>
2016-07-11 13:44:37 +0200

    Update Chart(Interface)

--- Chart.java
+++ Chart.java
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 package projekt7;
 
-import java.io.File;
 
 import javafx.scene.Scene;
 import javafx.scene.chart.*;
-import javafx.stage.Stage;
 
 interface Chart{
-	static File file = new File("TrackingData.txt");
-	static Stage stage = new Stage();
-	
 	
 	int leseZeiten(int info);	//Liest aus der durch Tracking erstellten Textdatei die Zeiten der Phasen 
 								//aus. Je nach übergebenem int wird "red" = 1 zurückgegeben, "green" = 2 usw.
@@ -21,77 +13,7 @@ interface Chart{
 	
 	Scene erstelleScene();			//Fügt den PieChart zu einer Scene hinzu und gibt diese zurück
 	
-	String setzteTitel();				//Gibt die übergebene Scene auf der Stage aus
+	String gebeProzenteAus();				//Gibt die übergebene Scene auf der Stage aus
+	
+	double berechneProzente(double i, double x);     //berechnet den Prozentwert von i im Grundwert x.
 }
-=======
-package projekt7;
-
-import java.io.File;
-
-import javafx.scene.Scene;
-import javafx.scene.chart.*;
-import javafx.stage.Stage;
-
-interface Chart{
-	static File file = new File("TrackingData.txt");
-	static Stage stage = new Stage();
-	
-	
-	int leseZeiten(int info);	//Liest aus der durch Tracking erstellten Textdatei die Zeiten der Phasen 
-								//aus. Je nach übergebenem int wird "red" = 1 zurückgegeben, "green" = 2 usw.
-
-	PieChart erstellePieChart();				//Erstellt aus den Zeiten ein PieChart 
-	
-	Scene erstelleScene(PieChart chart);			//Fügt den PieChart zu einer Scene hinzu und gibt diese zurück
-	
-	void gebeAus(Scene scene);				//Gibt die übergebene Scene auf der Stage aus
-}
->>>>>>> a55a274de2f524c3f9d0b9add30efd24ee2a4291
-=======
-package projekt7;
-
-import java.io.File;
-
-import javafx.scene.Scene;
-import javafx.scene.chart.*;
-import javafx.stage.Stage;
-
-interface Chart{
-	static File file = new File("TrackingData.txt");
-	static Stage stage = new Stage();
-	
-	
-	int leseZeiten(int info);	//Liest aus der durch Tracking erstellten Textdatei die Zeiten der Phasen 
-								//aus. Je nach übergebenem int wird "red" = 1 zurückgegeben, "green" = 2 usw.
-
-	PieChart erstellePieChart();				//Erstellt aus den Zeiten ein PieChart 
-	
-	Scene erstelleScene(PieChart chart);			//Fügt den PieChart zu einer Scene hinzu und gibt diese zurück
-	
-	void gebeAus(Scene scene);				//Gibt die übergebene Scene auf der Stage aus
-}
->>>>>>> origin/chart
-=======
-package projekt7;
-
-import java.io.File;
-
-import javafx.scene.Scene;
-import javafx.scene.chart.*;
-import javafx.stage.Stage;
-
-interface Chart{
-	static File file = new File("TrackingData.txt");
-	static Stage stage = new Stage();
-	
-	
-	int leseZeiten(int info);	//Liest aus der durch Tracking erstellten Textdatei die Zeiten der Phasen 
-								//aus. Je nach übergebenem int wird "red" = 1 zurückgegeben, "green" = 2 usw.
-
-	PieChart erstellePieChart();				//Erstellt aus den Zeiten ein PieChart 
-	
-	Scene erstelleScene(PieChart chart);			//Fügt den PieChart zu einer Scene hinzu und gibt diese zurück
-	
-	void gebeAus(Scene scene);				//Gibt die übergebene Scene auf der Stage aus
-}
->>>>>>> a55a274de2f524c3f9d0b9add30efd24ee2a4291
